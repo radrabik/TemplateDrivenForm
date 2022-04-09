8564a0f6011d10e983162cb18f669c98f56ac6c4
radrabik <radoslav.drabik@gmail.com>
2022-04-09 14:58:16 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,12 @@
-empimport { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  
+export class AppComponent {
   onLogin(loginForm: NgForm) {
-
-    console.log(loginForm.value, loginForm.valid)
-
+    console.log(loginForm.value, loginForm.valid);
   }
-
 }
